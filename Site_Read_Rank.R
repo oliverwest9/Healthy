fcf4953c94b417840e7769319a94a0f4ed0dfd7f
oliverwest9 <oliverwest9@googlemail.com>
2018-07-03 12:13:51 +0100

    Edited Out some Redundant Code.

--- Site_Read_Rank.R
+++ Site_Read_Rank.R
@@ -1,11 +1,6 @@
 ##data handling practice##
 
 #install requared pachages
-install.packages("tidyverse")
-install.packages("Matrix")
-install.packages("XLConnect")
-install.packages("readxl")
-install.packages("rJava")
 library(tidyverse)
 library(Matrix)
 library(XLConnect, pos = 4)
@@ -16,8 +11,6 @@ library(rJava)
 NSites <- 10
 Npopulation <- 10
 
-site_data <- read_xls("C:/Users/Alex/Documents/Exeter University/Internship/On Campus Placments/NHS/R/data_input.xls", Sheet=1)
-sheet.names <- getSheets(loadWorkbook('data_input.xls'))
 #load in data and adjust variables
 data_input <- read.csv(file="data_input.csv", header=TRUE) %>%
   mutate(site = factor(site))
@@ -42,5 +35,6 @@ for (i in 1:NSites){
 }
 #removing ranks that corisponde to NA's from the "distance" matrix
 rank_distance[rank_distance == NSites] <- NA
+
 
 
