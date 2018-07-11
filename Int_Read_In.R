72f8319490d532e8af5814b88656a1a00a717320
oliverwest9 <oliverwest9@googlemail.com>
2018-07-11 11:23:23 +0100

    Fixed Final Section, Primary Diagoses Saved per a year now work. Cost saved per a year, still need fixing.

--- Int_Read_In.R
+++ Int_Read_In.R
@@ -17,9 +17,11 @@ Years <- 5
 #Calculate Growth Rates. [Columns Labels, need to be sorted]
 for (i in 1:Years)
 {
-  Soc_Growth$Years <- round(Soc_Growth*data_input$Growth)
+  Soc_Growth$Years <- round(Soc_Growth*data_input$Growth);
 }
 
+#Short Term Fix, to unknown problem.
+Soc_Growth <- Soc_Growth[1,2] 
 
 #Calculate Follow Up Reductions Saved
 FU_Saved <- round(as.data.frame((data_input$FU_Redn)*(data_input$Y1)))
@@ -37,20 +39,33 @@ for (i in 1:Years)
   #Cost_Saved$Years <- round(Cost_Saved$
 }
 
-#Calculate Growth Rates for secondary effects of intiatives, [Tempremental, make sure you download excel input
-#sheet from Git]
-Diag_Saved_1 <- round(as.data.frame(data_input$Prob1*data_input$Y1))
-for (i in 2:Years)
+#Calculate Growth Rates for secondary effects of intiatives.
+Counter <- 1;
+Diag_Saved_1 <- round(as.data.frame(data_input$Y1*data_input$Prob1));
+colnames(Diag_Saved_1) <- ("Year_1");
+for (i in 1:Years)
 {
-  Diag_Saved_1[,i] <- round(Diag_Saved_1[,i-1]*Soc_Growth[,i])
+  Diag_Saved_1[,Counter+1] <- round(Soc_Growth[,Counter]*data_input$Prob1)
+  Counter <- Counter+1;
 }
-Diag_Saved_2 <- round(as.data.frame(data_input$Prob2*data_input$Y1))
-for (i in 2:Years)
+
+#Calculate Growth Rates for secondary effects of intiatives.
+Counter <- 1;
+Diag_Saved_2 <- round(as.data.frame(data_input$Y1*data_input$Prob2));
+colnames(Diag_Saved_2) <- ("Year_1");
+for (i in 1:Years)
 {
-  Diag_Saved_2[,i] <- round(Diag_Saved_2[,i-1]*Soc_Growth[,i])
+  Diag_Saved_2[,Counter+1] <- round(Soc_Growth[,Counter]*data_input$Prob2)
+  Counter <- Counter+1;
 }
-Diag_Saved_3 <- round(as.data.frame(data_input$Prob3*data_input$Y1))
-for (i in 2:Years)
+
+#Calculate Growth Rates for secondary effects of intiatives.
+Counter <- 1;
+Diag_Saved_3 <- round(as.data.frame(data_input$Y1*data_input$Prob3));
+colnames(Diag_Saved_3) <- ("Year_1");
+for (i in 1:Years)
 {
-  Diag_Saved_3[,i] <- round(Diag_Saved_3[,i-1]*Soc_Growth[,i])
+  Diag_Saved_3[,Counter+1] <- round(Soc_Growth[,Counter]*data_input$Prob3)
+  Counter <- Counter+1;
 }
+
